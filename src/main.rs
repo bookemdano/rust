b34959f3494ea5ab98e4d31e73a2d081363ffaa6
Dan Francis <dan.francis@school-pass.com>
2021-04-09 18:22:20 -0400

    Add roboguess

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,34 @@ fn main() {
     
     let secret_number = rand::thread_rng().gen_range(1, 101);
     // println!("The secret number is: {}", secret_number);
+    let mut roboguess = 50;
+    let mut delta = roboguess / 2;
+    let mut roboattempts = 0;
+    loop {
+        // println!("Roboguessed {} delta {}", roboguess, delta);
+        roboattempts += 1;
+        match roboguess.cmp(&secret_number) {
+            Ordering::Less => {
+                // too small
+                roboguess += delta;
+            },
+            Ordering::Greater => roboguess -= delta,
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+        delta = delta / 2;
+        if delta < 1
+        {
+            delta = 1;
+        }
+    }
+    println!("I guessed it in {} tries. Now you try.", roboattempts);
+    let mut attempts = 0;
     loop {
         println!("Enter a number to guess");
-        
+        attempts += 1;
         let mut guess = String::new();
         
         io::stdin()
@@ -34,5 +59,11 @@ fn main() {
             },
         }
         // println!("You guessed: {}", guess);
+    }
+
+    match roboattempts.cmp(&attempts) {
+        Ordering::Less => println!("I guessed faster!"),
+        Ordering::Greater => println!("You guessed faster!"),
+        Ordering::Equal => println!("Tie!"),
     }
 }

c9789e0d7cddba1e5cb9a7ddc52da33e79064d77
Dan <dan.francis@school-pass.com>
2021-04-15 07:36:32 -0400

    Rust 6.2

--- rust6/src/main.rs
+++ rust6/src/main.rs
@@ -38,4 +38,39 @@ fn main() {
     println!("absent_number {:?}", absent_number);
     println!("absent_number {:?}", absent_number.unwrap());
     
+
+    //6.2
+    let coin = Coin::Penny;
+    println!("{:?} value is {:?}", coin, value_in_cents(&coin));
+    let coin = Coin::Quarter(UsState::Alabama);
+    println!("{:?} value is {:?}", coin, value_in_cents(&coin));
+}
+
+#[derive(Debug)] 
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+#[derive(Debug)]
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: &Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
 }
